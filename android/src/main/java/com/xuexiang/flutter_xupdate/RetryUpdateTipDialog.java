2d226258bf4e9d603fe29e2913724c07671a24c9
lxs <858854440@qq.com>
2025-09-04 17:55:20 +0800

    refactor: 重构代码并迁移至 Gitee
    - 删除了 RetryUpdateTipDialog 类中的冗余注释和代码
    - 更新了 pubspec.yaml 中的项目信息，将 GitHub 链接替换为 Gitee 链接
    - 调整了 Flutter SDK 和 Dart SDK 的版本要求

--- android/src/main/java/com/xuexiang/flutter_xupdate/RetryUpdateTipDialog.java
+++ android/src/main/java/com/xuexiang/flutter_xupdate/RetryUpdateTipDialog.java
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2019 xuexiangjys(xuexiangjys@163.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 package com.xuexiang.flutter_xupdate;
 
 import android.app.AlertDialog;
@@ -29,25 +12,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.xuexiang.xupdate.XUpdate;
 
-
-/**
- * 版本更新重试提示弹窗
- *
- * @author xuexiang
- * @since 2019-06-15 00:06
- */
 public class RetryUpdateTipDialog extends AppCompatActivity implements DialogInterface.OnDismissListener {
 
     public static final String KEY_CONTENT = "com.xuexiang.flutter_xupdate.KEY_CONTENT";
     public static final String KEY_URL = "com.xuexiang.flutter_xupdate.KEY_URL";
 
-
-    /**
-     * 显示版本更新重试提示弹窗
-     *
-     * @param content
-     * @param url
-     */
     public static void show(String content, String url) {
         Intent intent = new Intent(XUpdate.getContext(), RetryUpdateTipDialog.class);
         intent.putExtra(KEY_CONTENT, content);
@@ -60,7 +29,6 @@ public class RetryUpdateTipDialog extends AppCompatActivity implements DialogInt
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         String content = getIntent().getStringExtra(KEY_CONTENT);
         final String url = getIntent().getStringExtra(KEY_URL);
 
@@ -70,25 +38,14 @@ public class RetryUpdateTipDialog extends AppCompatActivity implements DialogInt
 
         AlertDialog dialog = new AlertDialog.Builder(this)
                 .setMessage(content)
-                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        goWeb(url);
-                    }
-                })
+                .setPositiveButton(android.R.string.yes, (d, which) -> goWeb(url))
                 .setNegativeButton(android.R.string.no, null)
                 .setCancelable(false)
                 .show();
         dialog.setOnDismissListener(this);
     }
 
-    /**
-     * 以系统API的方式请求浏览器
-     *
-     * @param url
-     */
-    public void goWeb(final String url) {
+    private void goWeb(final String url) {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         try {
             startActivity(intent);
